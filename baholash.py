7761333e3f8c86a5b5a96e0976a0102ebaa0a68b
Dilshodbek <dilshodbekahmedov2615@gmail.com>
2021-09-17 23:56:37 +0500

    bahola ga yangicha kod
    oldingi bahola fayilimiz bilan buni nima farqi borligini topishingiz uchun

---
+++ baholash.py
@@ -0,0 +1,12 @@
+def bahola(isimlar):
+    baholar = {}
+    while isimlar:
+        ism = isimlar.pop()
+        baho = input(f"Talaba {ism.title() } ning bahosi: ")
+        baholar[ism] = int(baho)
+    return baholar
+talabalar = ["Shaxzod","fozil","sardor","Samiddin"]
+baholari = bahola(talabalar[:])
+print(baholari)
+print(talabalar)
+        
